a3ffbd50014d74c63d3625cb73b4ec4e14955e25
KingoBoiii <kingodk@outlook.dk>
2021-11-12 12:04:41 +0100

    Added tailwindcss

--- Vue/todo-list/src/App.vue
+++ Vue/todo-list/src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo List</h1>
+    <div>
+      <form  @submit.prevent="addTodo()">
+        <h1>New Todo: </h1>
+        <input v-model="newTodo" name="newTodo" autocomplete="off" placeholder="Add Todo...">
+        <button>+ Add</button>
+      </form>
+    </div>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <span :class="{done: todo.done}" @click="doneTodo(todo)">
+          {{ todo.content }}
+        </span>
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+    <h4 v-if="todos.length === 0">Empty list...</h4>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const newTodo = ref("");
+    const defaultData = [
+      { done: false, content: 'Tilføj noget' }
+    ];
+    const todosData = JSON.parse(localStorage.getItem('todos')) || defaultData;
+    const todos = ref(todosData);
+
+    function addTodo() {
+      if(newTodo.value) {
+        todos.value.push({
+          done: false,
+          content: newTodo.value
+        });
+        newTodo.value = '';
+      }
+      saveData();
+    }
+
+    function doneTodo(todo) {
+      todo.done = !todo.done;
+      saveData();
+    }
+
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+      saveData();
+    }
+
+    function saveData() {
+      const storage = JSON.stringify(todos.value);
+      localStorage.setItem('todos', storage);
+    }
+
+    return {
+      todos,
+      newTodo,
+      addTodo,
+      doneTodo,
+      removeTodo,
+      saveData
+    };
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
