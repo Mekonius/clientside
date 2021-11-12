55caf20f755c740c227fe41877903f6f636220a1
Your Name <you@example.com>
2021-11-12 12:47:56 +0100

    end of day commit

---
+++ Vue/todo_list/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <div>
+      <form @submit.prevent="addTodo()">
+        <h1>New Todo: </h1>
+        <input v-model="newTodo" name="newTodo" autocomplete="off" type="text" placeholder="add todo item..." />
+        <!-- <p>{{newTodo}}</p> -->
+        <button>+ tilføj</button>
+        </form>
+    </div>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index"> 
+        <span :class="{done: todo.done}" @click="doneTodo(todo)">
+          {{todo.content}}</span>
+          <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+    <h4 v-if="todos.length === 0">Tom liste...</h4>
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  export default {
+    name: "App",
+    setup() {
+      
+      //
+      const newTodo = ref("");
+      const defaultData = [
+        { 
+          done: false,
+          content: "Tilføj noget"
+        },
+      ];
+      const todosData = JSON.parse(localStorage.getItem("todos")) || defaultData;
+      const todos = ref(todosData);
+
+      //
+      function addTodo() {
+        if(newTodo.value) {
+          todos.value.push({
+            done: false,
+            content: newTodo.value,
+          });
+          newTodo.value = "";
+        }
+        saveData();
+      }
+
+      function doneTodo(todo) {
+        todo.done = !todo.done;
+      }
+
+      function removeTodo(index) {
+        todos.value.splice(index, 1);
+        saveData();
+      }
+
+      function saveData() {
+        const storage = JSON.stringify(todos.value);
+        localStorage.setItem("todos", storage);
+      }
+
+      return { 
+        todos,
+        newTodo,
+        addTodo,
+        doneTodo,
+        removeTodo,
+        saveData,
+      };
+
+    },
+  };
+</script>
