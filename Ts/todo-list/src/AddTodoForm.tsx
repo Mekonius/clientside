351aad8a3a62e31c3fd4f6eebe8cdb576dab14dd
Kenneth Løvgren Thomsen <kenneth.l.thomsen@gmail.com>
2021-11-12 10:17:31 +0100

    Working todo Form

--- Ts/todo-list/src/AddTodoForm.tsx
+++ Ts/todo-list/src/AddTodoForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
     addTodo: AddTodo;
@@ -6,10 +6,27 @@ interface Props {
 
 
 export const AddTodoForm: React.FC<Props> = ({ addTodo }) => {
+    const [text, setText] = useState('');
+    
     return (
         <form>
-            <input type="text" />
-            <button type="submit">Add Todo Item</button>
+            <input
+                type="text"
+                value={text}
+                onChange={e => {
+                    setText(e.target.value);
+                }}
+            />
+            <button
+                type="submit"
+                onClick={e => {
+                    e.preventDefault();
+                    addTodo(text);
+                    setText('');
+                }}
+                >
+                    Add Todo
+                </button>
         </form>
     )
 }
