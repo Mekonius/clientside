40377413aef5ac3dddb49c54977258df84d51ea0
Your Name <you@example.com>
2021-11-12 10:12:44 +0100

    added comment to force commit

--- Ts/todo-list2/src/AddTodoForm.tsx
+++ Ts/todo-list2/src/AddTodoForm.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
     addTodo: AddTodo;
 }
 
+// comment
+
 export const AddTodoForm: React.FC<Props> = ({addTodo}) => {
+
+    const [text, setText] = useState('');
+
     return (
         <form>
-            <input type="text" />
-            <button type="submit">Add Todo item</button>
+            <input 
+                type="text"
+                value={text}
+                onChange={e => { 
+                    setText(e.target.value);
+                }}
+            />
+            <button
+                type="submit"
+                onClick={e => {
+                    e.preventDefault();
+                    addTodo(text);
+                    setText('');
+                }}
+                >
+                    Add Todo
+                </button>
         </form>
     )
 }
