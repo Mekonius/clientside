98f456d91f08d7aa9f36aa0fbe5f74b66b0f1610
Your Name <you@example.com>
2021-11-15 09:43:25 +0100

    Added tailwind, working

--- Vue/todo_list/postcss.config.js
+++ Vue/todo_list/postcss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./public/**/*.html', './src/**/*.vue'],
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
